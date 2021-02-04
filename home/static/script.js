a0e9e1fed56bc777beba5469fc56dd725c8d57c9
shaw8wit <shaw8wit@gmail.com>
2021-02-05 00:18:51 +0530

    ui designed

--- home/static/script.js
+++ home/static/script.js
@@ -1,1 +1,8 @@
-console.log("yaay");
+function saveMeme() {
+    const username = document.getElementById('username').value;
+    const caption = document.getElementById('caption').value;
+    const url = document.getElementById('url').value;
+    document.querySelector('form').reset();
+    console.log(username, caption, url);
+    return false;
+}
