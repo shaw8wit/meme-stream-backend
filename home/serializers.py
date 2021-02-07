8da1834726e46355f46cdc03f24f124154e59d7a
shaw8wit <shaw8wit@gmail.com>
2021-02-07 21:10:47 +0530

    added different serializers

--- home/serializers.py
+++ home/serializers.py
@@ -3,6 +3,17 @@ from .models import Meme
 
 
 class MemeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Meme
+        fields = '__all__'
+        extra_kwargs = {"name": {"required": False, "allow_null": True},
+                        "url": {"required": False, "allow_null": True},
+                        "caption": {"required": False, "allow_null": True}}
+
+
+class MemesSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Meme
         fields = '__all__'
