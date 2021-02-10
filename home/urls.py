37bbbecf28da85595003c22b486f7cf5c2f94a29
shaw8wit <shaw8wit@gmail.com>
2021-02-10 12:56:16 +0530

    bug fixes

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
 
     # ? api endpoints
     path('memes', views.memes, name='memes'),
